0782ae5e3662dda71460fce920e7ae5aaa1d3ab3
Simon250 <54866743+Simon250@users.noreply.github.com>
2019-12-13 11:55:10 -0500

    into the matrix

--- MonsterCharger(Proto)/Assets/Scripts/PlayerMovement.cs
+++ MonsterCharger(Proto)/Assets/Scripts/PlayerMovement.cs
@@ -10,7 +10,6 @@ public class PlayerMovement : MonoBehaviour
     public float runSpeed = 6.0f;
     public float jumpSpeed = 8.0f;
     public float gravity = 20.0f;
-
    
 
     private Vector3 moveDirection = Vector3.zero;
